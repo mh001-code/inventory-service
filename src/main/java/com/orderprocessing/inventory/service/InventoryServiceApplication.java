6ed2156c858335d54e4385ad1d7992054676ec4c
Márcio Henrique <marcio.henrique09@gmail.com>
2026-05-18 16:51:34 -0300

    chore: initial inventory-service setup

--- src/main/java/com/orderprocessing/inventory/service/InventoryServiceApplication.java
+++ src/main/java/com/orderprocessing/inventory/service/InventoryServiceApplication.java
@@ -1,4 +1,4 @@
-package inventory_service;
+package com.orderprocessing.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 }
